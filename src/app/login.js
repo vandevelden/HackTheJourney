64df189046fd94d5733899a87afee53471a5b993
vandevelden <vandevelden@gmx.us>
2019-11-09 14:01:12 +0000

    change name

--- src/app/login.js
+++ src/app/login.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { navigate } from "gatsby"
 
-// import {
-//   IdentityModal,
-//   useIdentityContext,
-// } from "react-netlify-identity-widget"
-// import "react-netlify-identity-widget/styles.css" // delete if you want to bring your own CSS
+import {
+  IdentityModal,
+  useIdentityContext,
+} from "react-netlify-identity-widget"
+import "react-netlify-identity-widget/styles.css" // delete if you want to bring your own CSS
 
 function Login() {
   const identity = useIdentityContext()
@@ -15,12 +15,12 @@ function Login() {
       <h1>Log in</h1>
       <button onClick={() => setDialog(true)}>log in</button>
 
-      {/* <IdentityModal
+      <IdentityModal
         showDialog={dialog}
         onCloseDialog={() => setDialog(false)}
         onLogin={user => navigate("/app/profile")}
         onSignup={user => navigate("/app/profile")}
-      /> */}
+      />
     </>
   )
 }
